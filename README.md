8ae4f2f07b2338eb5753f45c702394557a8933fc
Blake Williams <blake@blakewilliams.me>
2016-01-24 15:46:47 -0500

    Add attributes to elements

--- README.md
+++ README.md
@@ -20,9 +20,9 @@ extern crate straw;
 
 use straw::element::{Element, Renderable}
 
-let element = Element::new("div", vec![
-  Element::new("h1", "Hello".to_owned()),
-  Element::new("h1", "World".to_owned()),
+let element = Element::new("div", Some(vec![("id".to_owned(), "main".to_owned())]), vec![
+  Element::new("h1", None, "Hello".to_owned()),
+  Element::new("h1", None, "World".to_owned()),
 ]);
 
 element.render();
--- src/element.rs
+++ src/element.rs
@@ -4,12 +4,13 @@ pub trait Renderable {
 
 pub struct Element<T: Renderable> {
     tag: String,
+    attributes: Option<Vec<(String, String)>>,
     child: T
 }
 
 impl<T: Renderable> Renderable for Element<T> {
     fn render(&self) -> String {
-        format!("<{}>{}</{}>", self.tag.clone(), self.child.render(), self.tag.clone())
+        format!("<{}>{}</{}>", self.open_tag(), self.child.render(), self.tag.clone())
     }
 }
 
@@ -20,12 +21,29 @@ impl Renderable for String {
 }
 
 impl<T: Renderable> Element<T> {
-    pub fn new(tag: &str, child: T) -> Element<T> {
+    pub fn new(tag: &str, attributes: Option<Vec<(String, String)>>, child: T) -> Element<T> {
         let tag_name = tag.to_owned();
 
         Element {
             tag: tag_name,
+            attributes: attributes,
             child: child
+        }
+    }
+
+    fn open_tag(&self) -> String {
+        match self.attributes.clone() {
+            Some(attributes) => {
+                let attribute_list: Vec<String> = attributes.iter().map(|tuple| -> String {
+                    format!("{}=\"{}\"", tuple.0, tuple.1)
+                }).collect();
+
+                format!("{} {}", self.tag.clone(), attribute_list.join(""))
+            }
+
+            None => {
+                self.tag.clone()
+            }
         }
     }
 }
@@ -42,16 +60,22 @@ impl<T: Renderable> Renderable for Vec<Element<T>> {
 
 #[test]
 fn it_can_render_single_elements() {
-    let element = Element::new("div", "foo".to_string());
+    let element = Element::new("div", None, "foo".to_string());
     assert_eq!(element.render(), "<div>foo</div>");
 }
 
 #[test]
 fn it_can_render_vecs_of_elements() {
-    let element = Element::new("div", vec![
-        Element::new("h1", "Hello".to_owned()),
-        Element::new("p", "This is neat, huh?".to_owned())
+    let element = Element::new("div", None, vec![
+        Element::new("h1", None, "Hello".to_owned()),
+        Element::new("p", None, "This is neat, huh?".to_owned())
     ]);
 
     assert_eq!(element.render(), "<div><h1>Hello</h1><p>This is neat, huh?</p></div>");
+}
+
+#[test]
+fn it_can_render_attributes() {
+    let element = Element::new("div", Some(vec![("id".to_owned(), "awesome".to_owned())]), "foo".to_string());
+    assert_eq!(element.render(), "<div id=\"awesome\">foo</div>");
 }
